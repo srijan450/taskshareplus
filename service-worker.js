2afe3e3d2be4fbc00c643f9e52cdd882242d67f0
Srijan Shankar Dubey <srijanshankardubey99@gmail.com>
2022-05-20 05:07:56 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/best-task-app/best-task-app/precache-manifest.1eabc9e97e3f592a91b6b7b3378158f3.js"
+  "./precache-manifest.5d39373d584995d645ce7795bd007c00.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/best-task-app/best-task-app/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
