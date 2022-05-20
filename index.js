60a04fb8665a063491e457f2d7654e95f1fbb02b
Srijan Shankar Dubey <srijanshankardubey99@gmail.com>
2022-05-20 17:25:17 +0530

    update in cors url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bodyparser = require("body-parser");
 const port = process.env.PORT || 5000;
 const app = express();
 const options = {
-    origin: 'https://srijan450.github.io/best-task-app',
+    origin: 'https://srijan450.github.io',
     credentials: true,
     optionSuccessStatus: 200
 }
