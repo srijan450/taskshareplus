c77ee963bdcde039eda0ffd97813042371cf1128
srijan shankar dubey <srijanshankardubey99@gmail.com>
2024-10-11 01:47:07 +0530

    updated database and email user

--- database/connection.js
+++ database/connection.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
-require('dotenv').config()
+require("dotenv").config();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.78mk3.mongodb.net/task_app?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => {
-        console.log("connected to database!");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@taskappdatabase.a9vi0.mongodb.net/?retryWrites=true&w=majority&appName=TaskAppDataBase`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("connected to database!");
+  })
+  .catch((err) => {
+    console.log("db conection error", err);
+  });
